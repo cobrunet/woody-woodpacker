e8be7f91157f011f9bc4fd86a581245a48f0e68a
cobrunet <cobrunet@student.42.fr>
2017-11-17 11:05:49 -0500

    structure de base d'ouverture de fichier

--- inc/woody.h
+++ inc/woody.h
@@ -2,5 +2,11 @@
 # define WOODY_H
 
 # include <unistd.h>
+# include <stdio.h>
+# include <sys/mman.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+
+void	*get_ptr(char *file, off_t *st_size);
 
 #endif
--- src/woody.c
+++ src/woody.c
@@ -1,6 +1,43 @@
 #include "woody.h"
 
-void	main()
+int		exit_failure(char *err_message)
 {
+	perror(err_message);
+	return (1);
+}
+
+void	*get_ptr(char *file, off_t *st_size)
+{
+	struct stat	buf;
+	int			fd;
+	void		*ptr;
+
+	if ((fd = open(file, O_RDWR)) < 0)
+		return (NULL);
+	if (fstat(fd, &buf) < 0)
+		return (NULL);
+	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE,
+		fd, 0)) == MAP_FAILED)
+		return (NULL);
+	*st_size = buf.st_size;
+	if (close(fd))
+		return (NULL);
+	return (ptr);
+}
+
+int		main(int ac, char **av)
+{
+	void	*ptr;
+	off_t	st_size;
+	int		i;
+
+	i = ac;
+	while (--i > 0)
+	{
+		if (!(ptr = get_ptr(av[i], &st_size)))
+			return (exit_failure(av[i]));
+		if (munmap(ptr, st_size) < 0)
+			return (exit_failure(""));
+	}
 	return (0);
 }

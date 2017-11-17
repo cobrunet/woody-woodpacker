f35854ad96562313f5d1c6a28e2088b900a0d54f
Constant BRUNET <cobrunet@e3r2p18.42.fr>
2017-11-17 17:37:14 +0100

    test cast

--- inc/woody.h
+++ inc/woody.h
@@ -6,7 +6,9 @@
 # include <sys/mman.h>
 # include <fcntl.h>
 # include <sys/stat.h>
+# include <elf.h>
 
 void	*get_ptr(char *file, off_t *st_size);
+int		exit_failure(char *err_message);
 
 #endif
---
+++ src/handle.c
@@ -0,0 +1,8 @@
+#include "woody.h"
+
+void	print_header(void *ptr)
+{
+	struct Elf64_Ehdr	*hdr;
+
+	hdr = ptr;
+}
